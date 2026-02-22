2f6dc1595dcd18d04c4de907b80d4a81ea44e19d
alihossen4 <mdahossen.dev@gmail.com>
2026-02-23 01:45:12 +0600

    feat: header section changes & component added

---
+++ app/components/header.tsx
@@ -0,0 +1,60 @@
+
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/logo/logo.png";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
+import { FaUser } from "react-icons/fa"
+import { useEffect, useState } from "react";
+export default function Header() {
+  const [isScroll, setScroll] = useState(false);
+  const handleScroll = () =>{
+    const scrolled = window.scrollY;
+    setScroll(scrolled>100);
+  }
+  useEffect(()=>{
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  const searchClick = () =>{
+
+  }
+  
+  return(
+    <>
+    {/* // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"> */}
+    <header className={`fixed top-0 left-0 right-0 z-50 py-2 transition-all ease-in-out ${isScroll? "bg-[#2b332f]  h-[180px]":" py-2"}`}>
+     <div className="container">
+      <div className="flex space-between">
+        <div className="logo">
+
+        </div>
+        <div className="menu m-0">
+          <ul className="flex menu-ul font-xl ">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/products">Products</Link></li>
+            <li><Link href="/categories">Categories</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/contact">Contact Us</Link></li>
+          </ul>
+        </div>
+        <div className="cart-section flex my-auto pt-5 gap-5">
+        <div className="search relative mr-34 ">
+
+          <input type="text" className="search-input absolute top-[-15] p-0 text-[18px] text-[#fff]" />
+          <FiSearch className="text-[#fff] h-[35px] w-[35px] absolute top-0 left-25"/>
+        </div>
+
+          <FiShoppingCart className="text-[#ffb8ab] h-[35px] w-[35px]"/>
+          <FaUser className="h-[35px] w-[35px] text-[#08803e]"/>
+        </div>
+     {/* </div> */}
+      </div>
+     
+    </div>
+    </header>
+  
+  </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,59 +4,29 @@ import Link from "next/link";
 import logo from "../public/logo/logo.png";
 import { FiSearch, FiShoppingCart } from "react-icons/fi";
 import { FaUser } from "react-icons/fa"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Header from "./components/header";
 export default function Home() {
-useEffect(()=>{
-const handleScroll = () =>{
-    const header = document.querySelector('header');
-    if(window.scrollY> 0){
-    header?.classList.add('scrolled');
-    }
-    else{
-      header?.classList.remove('scrolled');
-    }
-  document.addEventListener('scroll', handleScroll);
-  return () => document.removeEventListener("scroll", handleScroll);
-};
-}, []);
-const searchClick = () =>{
+  const [isScroll, setScroll] = useState(false);
+  const handleScroll = () =>{
+    const scrolled = window.scrollY;
+    setScroll(scrolled>100);
+  }
+  useEffect(()=>{
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  const searchClick = () =>{
 
-}
+  }
   
   return(
     <>
-    {/* // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"> */}
     <section className="menubar-section">
-     <div className="container">
-    <header >
-      <div className="flex">
-        <div className="logo">
-
-        </div>
-        <div className="menu">
-          <ul className="flex menu-ul font-xl ">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/products">Products</Link></li>
-            <li><Link href="/categories">Categories</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/contact">Contact Us</Link></li>
-          </ul>
-        </div>
-        <div className="cart-section flex my-auto pt-5 gap-5">
-          <FiSearch className="text-[#fff] h-[35px] w-[35px] search" onClick={()=>{
-            const search = document.querySelector('.search');
-            search?.classList.add('.search-clicked');
-            <>
-              <input type="text" className="search-input"/>
-            </>
-          }}/>
-          <FiShoppingCart className="text-[#ffb8ab] h-[35px] w-[35px]"/>
-          <FaUser className="h-[35px] w-[35px] text-[#08803e]"/>
-        </div>
-      </div>
-     {/* </div> */}
-    </header>
-      <div className="heading-section w-300 h-200 mt-50">
+      <Header />
+      <div className="container">
+      <div className="heading-section w-300 h-200 mt-80">
       <div className="heading text-xl">
         <h1>Exclusive Deals of Furniture Collection</h1>
       </div>
