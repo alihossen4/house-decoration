d64df85276478e65fac4c8a4982bec26bad9e17d
alihossen4 <mdahossen.dev@gmail.com>
2026-02-26 05:40:50 -0800

    images added

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -3,7 +3,9 @@ import bedroom from "@/public/fronts-room/dining-room.jpg";
 import MyImage from "./CartImage";
 import React from "react";
 export default function Category(){
-
+const listItem= [
+    {id:1, name:""}
+]
 type Props = {
     name: string;
     children: React.ReactNode;
@@ -26,12 +28,14 @@ const Cart =({name,children}:Props)=>{
                 <div className="container">
                         <h1 className="center my-5 mx-auto">Explore by Category</h1>
                     <div className="flex">
+                        <div className="search-item">
+                            
+                        </div>
                         <div className="carts mt-20">
                             <Cart name="Bedroom">
                                 <MyImage src={bedroom} alt="bedroom" width={200} height={250}/>
                             </Cart>
                         </div>
-                        <div className="search-item"></div>
                     </div>
                 </div>
             </section>
